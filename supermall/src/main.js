b561bdc840e2df62cc7a10970466cdeea76738dc
Bonny-lin-yiou <1521610172@qq.com>
2020-03-01 14:28:41 +0800

    code

---
+++ supermall/src/main.js
@@ -0,0 +1,26 @@
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+
+import toast from 'components/common/toast'
+import Fastclick from 'fastclick'
+import VueLazyLoad from 'vue-lazyload'
+
+//解决移动端300ms延迟
+Fastclick.attach(document.body)
+
+Vue.config.productionTip = false
+// 安装toast插件
+Vue.use(toast)
+
+//安装懒加载插件
+Vue.use(VueLazyLoad)
+
+Vue.prototype.$bus = new Vue()
+
+new Vue({
+  render: h => h(App),
+  router,
+  store
+}).$mount('#app')
